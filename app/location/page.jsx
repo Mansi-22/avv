222488f0a13af9f0a0f522fec8bce435638b02e7
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-07-31 23:02:01 +0530

    day1-tasks till  7
    1.navigation component
    2.api fetch of dummy data
    3.show data on page and a add form with save and cancel button

---
+++ app/location/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from'next/link'
+
+export const Location = () => {
+  return (
+    <div>Location 
+        <Link href="/">Admin Panel</Link>
+    </div>
+  )
+}
+
+export default Location;
---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage </h1>
+      <ul>
+      <li><Link href="/advertisement">Advertisement</Link></li>
+      <li><Link href="/cuisines">Cuisines</Link></li>
+      <li><Link href="/location">Location</Link></li>
+      <li><Link href="/pages">Pages</Link></li>
+      <li><Link href="/restaurants">Restaurants</Link></li>
+      <li><Link href="/settings">Settings</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Homepage;
