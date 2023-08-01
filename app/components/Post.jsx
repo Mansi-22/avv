43d9243bc431b8b67324c8a9c67605797ee2ebd9
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-08-01 21:35:14 +0530

    Update Post.jsx
    added delete but not working

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const Post = ({post}) => {
     const [openModalEdit, setModalOpenEdit] = useState(false);
     const [postToEdit,setPostToEdit] =useState(post);
+    const[openModalDelete,setOpenModalDelete] = useState(false);
 
     const handleEditSubmit =(e) => {
         e.preventDefault();
@@ -17,7 +18,10 @@ const Post = ({post}) => {
         const name = e.target.name;
         const value = e.target.value;
         setPostToEdit(prevState => ({...prevState, [name]: value}));
-    }            
+    }  
+    const handleDeletePost=(id) => {
+        console.log("deleted");
+    }          
     
   return (
     <li className='p-3 my-5 bg-slate-200' key={post.id}>
@@ -47,7 +51,14 @@ const Post = ({post}) => {
                 <button type="submit" className='bg-blue-700 text-white px-5 py-2' onClick={() => setModalOpenEdit(false)}>Cancel</button>
             </form>
         </Modal>
-        <button className='text-red-700 mr-3'>Delete</button>
+        <button onClick={() => setOpenModalDelete(true)} className='text-red-700 mr-3'>Delete</button>
+        <Modal modalOpen={openModalDelete} setModalOpen={setOpenModalDelete}>
+	        <h1 className="text-2xl pb-3"> Are you sure of this action?</h1>
+            <div>
+	        <button onClick={() => handleDeletePost(post.id)} classname="text-blue-700 font-bold mr-5">YES</button>
+	        <button onClick={() => setOpenModalDelete(false)} classname="text-red-700 font-bold mr-5">NO</button>	
+            </div>
+        </Modal>
     </div>
     </li>
   )
