1828330f055b86abbab263a067901774d1bc1a44
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-08-10 16:36:46 +0530

    done:UD for api
    update and delete operation for restaurant implemented  in backend.

---
+++ app/api/ress/[id]/route.js
@@ -0,0 +1,65 @@
+// url: http://localhost:3000/api/ress/12345
+import prisma from "@/app/libs/prismadb"
+import { NextResponse } from "next/server"
+
+export const GET = async (request, { params }) => {
+    try {
+        const { id } = params;
+        const restaurant = await prisma.Restaurants.findUnique({
+            where: {
+                resid : parseInt(id)
+            }
+        });   
+        if (!restaurant) {
+            return NextResponse.json(
+                {message: "restaurant not found", err},
+                {status: 404}
+            )
+        }
+
+    return NextResponse.json(restaurant);
+        } catch(err) {
+            return NextResponse.json({message: "GET it Error", err}, {status: 500})
+        }
+}
+
+export const PATCH = async (request, {params}) => {
+    try {
+        const body = await request.json();
+        const {name, description,address,resimage} = body;
+        const {id} = params;
+        const updateRestaurant = await prisma.Restaurants.update({
+            where: {
+                resid: parseInt(id)
+            },
+            data: {
+                name,
+                description
+                ,address,resimage
+            }
+            })
+        if (!updateRestaurant) {
+            return NextResponse.json(
+                {message: "Restaurant not found", err},
+                {status: 404}
+            )
+        }
+        return NextResponse.json(updateRestaurant);
+    } catch(err) {
+        return NextResponse.json({message: "update Error", err}, {status: 500})
+    }
+}
+
+export const DELETE = async (request, { params }) => {
+    try {
+        const { id } = params;
+        await prisma.Restaurants.delete({
+            where: {
+                resid : parseInt(id)
+            }
+        });   
+        return NextResponse.json("Restaurant has beeen deleted");
+    } catch(err) {
+        return NextResponse.json({message: "DELETE Error", err}, {status: 500})
+    }
+}
--- app/restaurants/Restaurant.jsx
+++ app/restaurants/Restaurant.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const restaurant = ({post}) => {
   return (
     <li className='p-3 my-5 bg-slate-200' key={post.id}>
-            <h1 className='text-2xl font-bold'>{post.name}</h1>
-            <p>{post.description}</p>
-            <p>{post.address}</p>
-            <p>{post.resimage}</p>
-            </li>
+      <h1 className='text-2xl font-bold'>{post.name}</h1>
+      <p>{post.description}</p>
+      <p>{post.address}</p>
+      <p>{post.resimage}</p>
+      
+    </li>
   )
 }
 
