1354fe31fbbd4d5c3a113c2181d90bd89d095e74
Mansi-22 <73896751+Mansi-22@users.noreply.github.com>
2023-08-10 11:19:09 +0530

    restaurant database created

--- app/restaurants/page.jsx
+++ app/restaurants/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Link from'next/link'
+import DropDown from '../components/DropDown';
 
 export const Restaurants = () => {
   return (
-    <div>Restaurants 
-        <Link href="/">Admin Panel</Link>
+    <div>
+      Restaurants <Link href="/">Admin Panel</Link>
+      
+      <DropDown/>
     </div>
   )
 }
